22bafa1318e7dcb47481e9d02513987a1a852b7e
Ulrik Bonde <ulrikbonde@gmail.com>
2011-07-05 02:22:45 +0200

    Fixed matlab aliases

---
+++ setup/setup.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python
+
+import os
+from os.path import join
+import errno
+
+# Program variables
+home = os.path.expanduser('~')
+dotfiles = 'dotfiles'
+
+
+# The dictionary of links to be created and targets.
+# Link names are relative to users home.
+# Targets are a tuple (dir,ans) where ans is the stabdard answer.
+# Target dirs are relative to the dotfiles dir.
+linkdict = { '.vimrc' : ('vimconf/vimrc','y'),
+             '.vim' : ('vimconf/vim','y'),
+             '.zshrc' : ('zshconf/zshrc','y'),
+             '.latexmkrc' : ('latexmkrc','y'),
+             '.zope-external-edit' : ('zope-external-edit','n'),
+             '.Xdefaults' : ('Xdefaults','n')
+         }
+
+
+# Dictionaty for prompt inputs
+ansdict = { 'y' : True,
+            'n' : False
+        }
+
+
+# Format the default choices
+def format_choices(link):
+    ret = []
+    for c in ['y','n']:
+        if c is linkdict[link][1]:
+            ret.append(c.upper())
+        else:
+            ret.append(c)
+    return '/'.join(ret)
+
+
+# Prompt for input, returns true or false
+def prompt(link):
+    target = join(home, dotfiles, linkdict[link][0])
+    choices = format_choices(link)
+    ans = raw_input('Create %s to %s? [%s] ' % (link, target, choices))
+    if ans.lower() is '':
+        return ansdict[linkdict[link][1]]
+    elif ans.lower() in ansdict.keys():
+        return ansdict[ans.lower()]
+    else:
+        print 'Invalid selection, try again.'
+        return prompt(link)
+
+
+# Create the symbolic link to the target.
+def create_symlink(link):
+    linkpath = os.path.join(home,link)
+    targetpath = os.path.join(home, dotfiles, linkdict[link][0])
+    try:
+        os.symlink(targetpath, linkpath)
+    except OSError as e:
+        if e.errno is errno.EEXIST:
+            print "File %s exists!" % linkpath
+            print "Remove or rename existing file."
+        elif e.errno is errno.EACCESS:
+            print "Cannot create symlink %s!" % linkpath
+            print "Permission error!"
+        else:
+            print "Some error happened."
+            raise
+        return False
+    else:
+        return True
+
+
+# Run this thing
+def main():
+    for link in linkdict:
+        make_link = prompt(link)
+        made_link = False
+        if make_link:
+            made_link = create_symlink(link)
+
+        if make_link and made_link:
+            print 'Success.'
+        else:
+            print "Skipping..."
+        print ''
+
+
+msg = """
+dotfiles setup utility
+Very simple, just answer the questions.
+If at any time you want to abort use C-c and enjoy the exceptions.
+
+"""
+
+
+if __name__ == '__main__':
+    print msg
+    main()
---
+++ setup/setup_git.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# The test in the function set_value is rather
+# redundant since we always want to set the value
+# anyways. It's not like we get some huge
+# boost in performance :)
+
+
+function set_value {
+    val=`git config --global "$1"`
+    if [ "$val" != "$2" ]
+    then
+        git config --global --unset "$1"
+        git config --global "$1" "$2"
+    fi
+}
+
+
+user_name='Ulrik Bonde'
+user_email='ulrikbonde@gmail.com'
+
+gh_user='bonde'
+
+
+echo 'Setting up the git configuration'
+
+
+echo 'Enter your github API token (find it at https://github.com/account/admin)'
+read api_token
+
+
+# Set user name
+set_value "user.name" "$user_name"
+exit
+
+# Set user email
+set_value "user.email" "$user_email"
+
+# Set github info
+set_value "github.user" "$gh_user"
+set_value "github.token" "$api_token"
+
+# Set colors
+set_value "color.ui" "true"
+
+
+echo "Please review and clean up the config file if necessary."
+read null
+vim $HOME/.gitconfig
