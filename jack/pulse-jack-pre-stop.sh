2953eb32927b0b3b0c397f63bf1a142d3c924fc5
Ulrik Bonde <ulrikbonde@gmail.com>
2015-04-23 12:39:27 +0200

    Add some stuff for Ubuntu Studio

---
+++ jack/pulse-jack-pre-stop.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+NKID=$(pactl list | grep -B 1 "Name: module-jack-sink" | grep Module | sed 's/[^0-9]//g')
+SOURCEID=$(pactl list | grep -B 1 "Name: module-jack-source" | grep Module | sed 's/[^0-9]//g')
+pactl unload-module $SINKID
+pactl unload-module $SOURCEID
+killall a2jmidid
+sleep 5
